16190a7502ca5694e0fb72860bd11079f1e6784a
harsh021102 <harsh021102@gmail.com>
2023-04-30 00:32:41 +0530

    1:09:32

---
+++ src/container/Home.jsx
@@ -0,0 +1,11 @@
+import React, {useState, useRef, useEffect} from 'react'
+import {HiMenu} from 'react-icons/hi'
+import {AiFillClosedCircle} from 'react-icons/ai'
+import {Link,Route,Routes} from 'react-router-dom'
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
+      <Router>
+        <App />
+      </Router>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
