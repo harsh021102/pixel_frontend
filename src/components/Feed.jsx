0cad96db0bca13c4192613e1d2cfa21eefddd2d1
harsh021102 <harsh021102@gmail.com>
2023-05-12 03:02:14 +0530

    2:58:40 (Pin page completed)

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,11 +6,41 @@ import { feedQuery, searchQuery } from '../utils/data';
 import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
 
+
 const Feed = () => {
   const [loading,setLoading] = useState(false)
-  if(loading) return <Spinner message="We are adding new ideas to your feed!"/>
+  const [pins, setPins] = useState();
+  const {categoryId} = useParams()
+
+  useEffect(() => {
+    if (categoryId) {
+      setLoading(true);
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        // console.log(data);
+        setPins(data);
+        // console.log("1");
+        setLoading(false);
+      });
+    } else {
+      setLoading(true);
+      
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        // console.log("2");
+        // console.log(data);
+        // console.log(pins[0].image.asset.url);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
   return (
-    <div>Feed</div>
+    <div>
+      {/* {console.log(pins)} */}
+      {pins && (
+        <MasonryLayout pins={pins} />
+      )}
+    </div>
   )
 }
 
